b455ca02aa691878d8dd5ced39a68522c69c3f2a
ARTHUR CLAUDIO MONTEIRO MARTINS DA SILVA <arthur.silva@elo.com.br>
2021-08-10 17:57:19 -0300

    Initial commit

---
+++ src/main/java/com/example/testeci/TesteCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.testeci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TesteCiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/testeci/TesteCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
